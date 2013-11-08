479c7580ddaa53b1e405b15ab3d7c8c38fa22b14
Sebastian Ånerud <anerud@student.chalmers.se>
2013-11-08 21:18:45 +0100

    First classes and reading of data

---
+++ src/antColonyOpt/MainFFR105Data.java
@@ -0,0 +1,13 @@
+package antColonyOpt;
+
+public class MainFFR105Data {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new AntColonyOpt("C:\\Users\\Sebbe\\Documents\\GitHub\\Spare-time-project\\" +
+				"data files\\LoadCityLocations.txt");
+	}
+
+}
